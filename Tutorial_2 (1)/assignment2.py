cd1b6f3a2835f0ec3e39c0a133d4b2dc6f476c4b
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2021-05-02 16:51:18 +0200

    separable done

--- Tutorial_2 (1)/assignment2.py
+++ Tutorial_2 (1)/assignment2.py
@@ -2,66 +2,106 @@
 
 import numpy as np
 import pdb
+import math
 
-def imgmirror(M, w):
+def imgmirror(M, h):
     n, m = M.shape
+    w = math.floor(h/2)
     O = np.zeros((n+2*w, m+2*w))
     O[w:-w,w:-w] = M
     O[w:-w,:w] = M[:,w-1::-1] 
     O[w:-w,-w:] = M[:,:-w-1:-1] 
     O[:w,w:-w] = M[w-1::-1,:] 
     O[-w:,w:-w] = M[:-w-1:-1,:] 
-    '''
-    O[w:-w,:w]  = M[:,:w]
-    O[w:-w,-w:] = M[:,-w:]
-    O[:w,w:-w]  = M[:w,:]
-    O[-w:,w:-w] = M[-w:,:]
-    '''
     return O
 
-M = np.array([[ 1, 2], [3, 4]]) # (2,2)
-w = 2
-O = imgmirror(M, w) # (4,4)
-# 1.2
-import math
+# Test imgmirror function
+M = np.array([[1, 2], [3, 4]]) 
+h = 5
+O = imgmirror(M, h) 
 
+# 1.2
 def gaussfilter(I1, K):
     h, _ = K.shape
     n, m = I1.shape
-    a = math.ceil(h/2)
    
     I2 = np.empty_like(I1)
-    I1 = imgmirror(I1, h//2)
-    pdb.set_trace()
+    I1 = imgmirror(I1, h)
     for x in range(n):
         for y in range(m):
-            #pdb.set_trace()
-            I2[x,y] = (I1[x:x+h, y:y+h] * K).sum() #,y-a+1:y+a-1
-            #I2[x,y] += I1[x+i-a,y+j-a] * K[i,j]
-    pdb.set_trace()
+            I2[x,y] = (I1[x:x+h, y:y+h] * K).sum() 
     return I2
 
 # 1.3
-#from PIL import Image
 from matplotlib import pyplot as plt
 
-def gausskern(sig):
-    """
-    creates gaussian kernel with a sigma of sig
-    """
-    ax = np.linspace(-2., 2., 5)
+def gausskern(sigma):
+    ax = np.linspace(-2., 2., 5) #5x5 kernel
     xx, yy = np.meshgrid(ax, ax)
-    kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(sig))
+    kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(sigma))
     return kernel / np.sum(kernel)
 
-#I1 = Image.open('noisycoke.jpg')
 I1 = plt.imread('noisycoke.jpg')
-K = gausskern(1)
-I2 = gaussfilter(I1, K)
-plt.imshow(I2, cmap='gray')
 
+sigmas = [1]#[0.25, 0.5, 1, 2, 4]
+denoised_imgs = []
+
+for sig in sigmas:
+    K = gausskern(sig)
+    I2 = gaussfilter(I1, K)
+    denoised_imgs.append(I2)
+
+denoised_imgs = np.hstack(denoised_imgs)
+plt.imshow(denoised_imgs, cmap='gray')
+plt.savefig('denoisedcoke.jpg')
 
 # 1.4
+def gaussfilter_sep(I1, K):
+    '''
+    h = K.size
+    n, m = I1.shape
+    
+    pdb.set_trace()
+    I2x = np.empty([n, 5])
+    I2y = np.empty([5, m])
+    I1 = imgmirror(I1, h)
 
-def gaussfilter_sep():
-    pass    
+    Kx = np.broadcast_to()
+    for x in range(n):
+        I2x[x,:] = (I1[x:x+h,:] * K)
+    for y in range(m):
+        I2y[:,y] = I1[:,y:y+h] * K         
+    return I2
+    '''
+    pdb.set_trace()
+    img = I1
+    kSize = K.size
+    kernel = K
+    gausX = np.zeros((img.shape[0], img.shape[1] - kSize + 1))
+    for i, v in enumerate(kernel):
+    #for i, v in enumerate(kernel[0]):
+        gausX += v * img[:, i : img.shape[1] - kSize + i + 1]
+    gausY = np.zeros((gausX.shape[0] - kSize + 1, gausX.shape[1]))
+    #for i, v in enumerate(kernel[:,0]):
+    for i, v in enumerate(kernel):
+        gausY += v * gausX[i : img.shape[0]  - kSize + i + 1]
+    pdb.set_trace()
+    return gausY
+
+def gausskern_sep(sigma):
+    ax = np.linspace(-2., 2., 5) #5 kernel
+    kernel = np.exp(-0.5 * np.square(ax) / np.square(sigma))
+    return kernel / np.sum(kernel)
+
+sigmas = [1]#[0.25, 0.5, 1, 2, 4]
+denoised_imgs = []
+
+for sig in sigmas:
+    K = gausskern_sep(sig)
+    I2 = gaussfilter_sep(I1, K)
+    denoised_imgs.append(I2)
+
+denoised_imgs = np.hstack(denoised_imgs)
+plt.imshow(denoised_imgs, cmap='gray')
+pdb.set_trace()
+plt.savefig('denoisedcoke_sep.jpg')
