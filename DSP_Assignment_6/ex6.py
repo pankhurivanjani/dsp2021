b3fbbefdb521ea5f69082b6138caa6299e0fcede
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2021-05-31 15:12:30 +0200

    Complete complete!

--- DSP_Assignment_6/ex6.py
+++ DSP_Assignment_6/ex6.py
@@ -4,16 +4,12 @@ import matplotlib.pyplot as plt
 import math
 import pdb
 
-def gen_next_gt(px, py, vx, vy):
-    px = px + vx * dt
-    py = py + vy * dt 
-    return px, py, vx, vy
-
 def gen_next_measurement(px, py, vx, vy, ay, dt):
     px = px + vx * dt
     py = py + vy * dt + 0.5 * ay * (dt)**2
     vy = vy + ay * dt
     return px, py, vx, vy
+
 
 # 2.2
 def add_noise(px, py, vx, vy, mu, var):
@@ -24,107 +20,148 @@ def add_noise(px, py, vx, vy, mu, var):
     vy += np.random.normal(mu, std)
     return px, py, vx, vy 
 
-def get_noise(mu, var):
-    std = math.sqrt(var)
-    noise = np.diag([np.random.normal(mu, std), np.random.normal(mu, std), np.random.normal(mu, std), np.random.normal(mu, std)])
-    return noise
+# 2.3 and 2.4
 
-# 2.3
-ay = -3.7
-dt = 0.01
-mu = 0
-var_eta = 1.
-var_gamma = 0.01
-N = 760
+def gen_next_gt(px, py, vx, vy, dt):
+    px = px + vx * dt
+    py = py + vy * dt 
+    return px, py, vx, vy
 
-px0, py0 = (0, 0)
+def kalman_filter(var_eta, var_gamma):
+    ay = -3.7
+    dt = 0.01
+    mean = 0
+    N = 760
 
-v = 20 
-alpha = 45
-vx0 = v * math.cos(math.pi * alpha / 180)
-vy0 = v * math.sin(math.pi * alpha / 180)
+    px0, py0 = (0, 0)
 
-#pdb.set_trace()
-(px, py, vx, vy) = (px0, py0, vx0, vy0)
-states_gt = [[px, py, vx, vy]]
-for i in range(N):
-    px, py, vx, vy = gen_next_gt(px, py, vx, vy)
-    px, py, vx, vy = add_noise(px, py, vx, vy, mu, var_gamma)
-    states_gt.append([px, py, vx, vy])
-states_gt = np.array(states_gt)
+    v = 20 
+    alpha = 45
+    vx0 = v * math.cos(math.pi * alpha / 180)
+    vy0 = v * math.sin(math.pi * alpha / 180)
 
+    (px, py, vx, vy) = (px0, py0, vx0, vy0)
+    states_gt = [[px, py, vx, vy]]
+    for i in range(N):
+        px, py, vx, vy = gen_next_gt(px, py, vx, vy, dt)
+        px, py, vx, vy = add_noise(px, py, vx, vy, mean, var_gamma)
+        states_gt.append([px, py, vx, vy])
+    states_gt = np.array(states_gt)
 
-(px, py, vx, vy) = (px0, py0, vx0, vy0)
-states_measurement = [[px, py, vx, vy]]
-for i in range(N):
-    px, py, vx, vy = gen_next_measurement(px, py, vx, vy, ay, dt)
-    px, py, vx, vy = add_noise(px, py, vx, vy, mu, var_eta)
-    states_measurement.append([px, py, vx, vy])
-states_measurement = np.array(states_measurement)
+    (px, py, vx, vy) = (px0, py0, vx0, vy0)
+    states_measurement = [[px, py, vx, vy]]
+    for i in range(N):
+        px, py, vx, vy = gen_next_measurement(px, py, vx, vy, ay, dt)
+        px, py, vx, vy = add_noise(px, py, vx, vy, mean, var_eta)
+        states_measurement.append([px, py, vx, vy])
+    states_measurement = np.array(states_measurement)
 
-(px, py, vx, vy) = (px0, py0, vx0, vy0)
-states_optimal = [[px, py, vx, vy]]
+    (px, py, vx, vy) = (px0, py0, vx0, vy0)
+    states_optimal = [[px, py, vx, vy]]
 
-A = np.array([[1, dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, dt], [0, 0, 0, 1]])
-b = np.array([0, 0, 0.5 * (dt**2), ay * dt])
-#std_gamma = math.sqrt(var_gamma)
-#gamma = np.diag([np.random.normal(mu, std_gamma), np.random.normal(mu, std_gamma), np.random.normal(mu, std_gamma), np.random.normal(mu, std_gamma)])
-gamma = np.identity(4) * var_gamma
-C = np.eye(4)
-#std_eta = math.sqrt(var_eta)
-#sigma = np.diag([np.random.normal(mu, std_eta), np.random.normal(mu, std_eta), np.random.normal(mu, std_eta), np.random.normal(mu, std_eta)])
-sigma = np.identity(4) * var_eta
-V = np.zeros((4, 4))
-mu = np.array([px, py, vx, vy])
+    A = np.array([[1, dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, dt], [0, 0, 0, 1]]) #dynamics
+    C = np.eye(4) #measurement process
 
-#TODO merge measurement, optimal and gt to single loop
-for i in range(N):
-    #pdb.set_trace()
-    #V = get_noise(mu, var_eta) #TODO what's V?
-    P = np.matmul(A, np.matmul(V, A.T)) + gamma #TODO if gamma should change at each iteration?
-    t = np.linalg.inv(np.matmul(np.matmul(C, P), C.T) + sigma)
-    K = np.matmul(np.matmul(P, C.T), t) 
-    px, py, vx, vy = gen_next_measurement(px, py, vx, vy, ay, dt)
-    #px, py, vx, vy = add_noise(px, py, vx, vy, mu, var_eta)
-    x = np.array([px, py, vx, vy])
-    tt = x - np.matmul(C, np.matmul(A, mu))
-    mu = np.matmul(A, mu) + np.matmul(K, tt)
-    states_optimal.append(mu)
-    V = 1 - np.matmul(np.matmul(K, C), P)
-    #C = 
-    #break
+    gamma = np.identity(4) * var_gamma # purturbation covarriance 
+    sigma = np.identity(4) * var_eta # noise covarriance 
+    V = np.zeros((4, 4)) # meausrement uncertainty
+    mu = np.array([px, py, vx, vy]) #initial position, velocity
 
-states_optimal = np.array(states_optimal)
+    kalman_gain = []
+    for i in range(N):
+        P = np.matmul(A, np.matmul(V, A.T)) + gamma 
+        #t = np.linalg.inv(np.matmul(np.matmul(C, P), C.T) + sigma)
+        K = np.matmul(np.matmul(P, C.T), np.linalg.inv(np.matmul(np.matmul(C, P), C.T) + sigma)) 
+        kalman_gain.append(np.diagonal(K))
+        px, py, vx, vy = gen_next_measurement(px, py, vx, vy, ay, dt)
+        x = np.array([px, py, vx, vy])
+        #tt = x - np.matmul(C, np.matmul(A, mu))
+        mu = np.matmul(A, mu) + np.matmul(K, x - np.matmul(C, np.matmul(A, mu)))
+        states_optimal.append(mu)
+        V = 1 - np.matmul(np.matmul(K, C), P)
+    states_optimal = np.array(states_optimal)
+    kalman_gain = np.array(kalman_gain)
 
-plt_px = plt.subplot(2, 2, 1)
-#plt.set_title("Original and overlaid predicted signal")
-plt_px.plot(states_gt[...,0], label = "Ground truth")
-plt_px.plot(states_measurement[...,0], label = "Measurement")
-plt_px.plot(states_optimal[...,0], label = "Optimal")
-plt.ylabel("X Position (m)")
+    plt.figure(figsize=(16, 8))    
+    
+    plt_px = plt.subplot(2, 2, 1)
+    plt_px.plot(states_gt[...,0], label = "Ground truth")
+    plt_px.plot(states_measurement[...,0], label = "Measurement")
+    plt_px.plot(states_optimal[...,0], label = "Optimal")
+    plt.ylim([-100, 200])
+    plt.ylabel("X Position (m)")
+    plt.xlabel("time (sec)")
+    plt.legend()
+    plt.title("2-D Kalman Filter with eta = {} and sigma = {}".format(var_eta, var_gamma))
+
+    plt_py = plt.subplot(2, 2, 2)
+    plt_py.plot(states_gt[...,1], label = "Ground truth")
+    plt_py.plot(states_measurement[...,1], label = "Measurement")
+    plt_py.plot(states_optimal[...,1], label = "Optimal")
+    plt.ylim([-100, 100])
+    plt.ylabel("Y Position (m)")
+    plt.xlabel("time (sec)")
+
+    plt_vx = plt.subplot(2, 2, 3)
+    plt_vx.plot(states_gt[...,2], label = "Ground truth")
+    plt_vx.plot(states_measurement[...,2], label = "Measurement")
+    plt_vx.plot(states_optimal[...,2], label = "Optimal")
+    plt.ylim([-100, 100])
+    plt.ylabel("X Velocity (m)")
+    plt.xlabel("time (sec)")
+
+    plt_vy = plt.subplot(2, 2, 4)
+    plt_vy.plot(states_gt[...,3], label = "Ground truth")
+    plt_vy.plot(states_measurement[...,3], label = "Measurement")
+    plt_vy.plot(states_optimal[...,3], label = "Optimal")
+    plt.ylim([-100, 100])
+    plt.ylabel("Y Velocity (m)")
+    plt.xlabel("time (sec)")
+
+    #plt.show()
+    plt.savefig('filter_{}_{}.jpg'.format(var_eta, var_gamma))
+    return kalman_gain
+
+var_eta = [1., 0.01, 1.] #[0.01]  
+var_gamma = [0.01, 0.01, 0.05] #[0.01]
+
+kalman_gains = []
+for i in range(len(var_eta)):
+    kalman_gain = kalman_filter(var_eta[i], var_gamma[i])
+    kalman_gains.append(kalman_gain)
+kalman_gains = np.array(kalman_gains)
+
+# 2.5
+plt.figure(figsize=(16, 8))    
+plt_d1 = plt.subplot(2, 2, 1)
+plt_d1.plot(kalman_gains[0,...,0], label = "Eta_{}_Gamma_{}".format(var_eta[0], var_gamma[0]))
+plt_d1.plot(kalman_gains[1,...,0], label = "Eta_{}_Gamma_{}".format(var_eta[1], var_gamma[1]))
+plt_d1.plot(kalman_gains[2,...,0], label = "Eta_{}_Gamma_{}".format(var_eta[2], var_gamma[2]))
+plt.ylabel("K diagonal element 1")
 plt.xlabel("time (sec)")
 plt.legend()
+plt.title("2-D Kalman Filter Gain diagonal elements for various eta and sigma")
 
-plt_py = plt.subplot(2, 2, 2)
-plt_py.plot(states_gt[...,1], label = "Ground truth")
-plt_py.plot(states_measurement[...,1], label = "Measurement")
-plt_py.plot(states_optimal[...,1], label = "Optimal")
-plt.ylabel("Y Position (m)")
+plt_d2 = plt.subplot(2, 2, 2)
+plt_d2.plot(kalman_gains[0,...,1], label = "Eta_{}_Gamma_{}".format(var_eta[0], var_gamma[0]))
+plt_d2.plot(kalman_gains[1,...,1], label = "Eta_{}_Gamma_{}".format(var_eta[1], var_gamma[1]))
+plt_d2.plot(kalman_gains[2,...,1], label = "Eta_{}_Gamma_{}".format(var_eta[2], var_gamma[2]))
+plt.ylabel("K diagonal element 2")
 plt.xlabel("time (sec)")
 
-plt_vx = plt.subplot(2, 2, 3)
-plt_vx.plot(states_gt[...,2], label = "Ground truth")
-plt_vx.plot(states_measurement[...,2], label = "Measurement")
-plt_vx.plot(states_optimal[...,1], label = "Optimal")
-plt.ylabel("X Velocity (m)")
+plt_d3 = plt.subplot(2, 2, 3)
+plt_d3.plot(kalman_gains[0,...,2], label = "Eta_{}_Gamma_{}".format(var_eta[0], var_gamma[0]))
+plt_d3.plot(kalman_gains[1,...,2], label = "Eta_{}_Gamma_{}".format(var_eta[1], var_gamma[1]))
+plt_d3.plot(kalman_gains[2,...,2], label = "Eta_{}_Gamma_{}".format(var_eta[2], var_gamma[2]))
+plt.ylabel("K diagonal element 3")
 plt.xlabel("time (sec)")
 
-plt_vy = plt.subplot(2, 2, 4)
-plt_vy.plot(states_gt[...,3], label = "Ground truth")
-plt_vy.plot(states_measurement[...,3], label = "Measurement")
-plt_vy.plot(states_optimal[...,1], label = "Optimal")
-plt.ylabel("Y Velocity (m)")
+plt_d4 = plt.subplot(2, 2, 4)
+plt_d4.plot(kalman_gains[0,...,3], label = "Eta_{}_Gamma_{}".format(var_eta[0], var_gamma[0]))
+plt_d4.plot(kalman_gains[1,...,3], label = "Eta_{}_Gamma_{}".format(var_eta[1], var_gamma[1]))
+plt_d4.plot(kalman_gains[2,...,3], label = "Eta_{}_Gamma_{}".format(var_eta[2], var_gamma[2]))
+plt.ylabel("K diagonal element 4")
 plt.xlabel("time (sec)")
 
-plt.show()
-
+#plt.show()
+plt.savefig('kalman_gain_coeff.jpg')
