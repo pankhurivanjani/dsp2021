16288ceafee093a4511a0ec91b72a2ac85eff8fe
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2021-06-14 14:26:47 +0200

    1.3 complete

--- DSP_Assignment_8/ex8.py
+++ DSP_Assignment_8/ex8.py
@@ -4,6 +4,7 @@ from scipy.fft import dct, idct
 import matplotlib.pyplot as plt
 import soundfile as sf
 import sounddevice
+from scipy.io import wavfile
 import pdb
 
 # 1.1 Pre-emphasis
@@ -13,7 +14,7 @@ def preemphasis(audiostream, alpha=0.95):
     return emph_audiostream
 
 point8, sample_rate = sf.read('DSP_Assignment_8/point8.au') # (485100, 8), 44100, 11 sec
-
+#sample_rate, point8 = wavfile.read('DSP_Assignment_8/point8.au') Doesn't work
 '''
 #sounddevice.play(point8[0], sample_rate)
 plt.figure(figsize=(16, 8))    
@@ -58,11 +59,52 @@ num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_ste
 pad_signal_length = num_frames * frame_step + frame_length
 z = np.zeros((pad_signal_length - signal_length, point8_emph.shape[1]))
 
-pdb.set_trace()
+#pdb.set_trace()
 pad_signal = np.append(point8_emph, z, axis=0) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
 
 indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T # [num_frames, frame_length]
 frames = pad_signal[indices.astype(np.int32, copy=False)]
 
 frames *= np.repeat(np.hamming(frame_length)[None], point8_emph.shape[1], axis=0).T
-#np.repeat(np.hamming(frame_length)[None], 8, axis=0).T.shape
+
+# 1.3 Mel-Filterbank
+def hz2mel(f_hz):
+    f_mel = 2595 * math.log10(1 + f_hz / 700)
+    return f_mel
+
+def mel2hz(f_mel):
+    f_hz = (700 * (10**(f_mel / 2595) - 1))
+    return f_hz
+
+def mel_filterbank(fl, fh, nfft, fs, L):
+    mel_fl = hz2mel(fl)
+    mel_fh = hz2mel(fh)
+    mel_points = np.linspace(mel_fl, mel_fh, L+2)
+    hz_points = mel2hz(mel_points)
+    f = np.floor((nfft + 1) * hz_points / fs) 
+    filter_bank = np.zeros((L, int(np.floor(nfft / 2 + 1)))) #TODO
+    
+    for m in range(1, L+1):
+        for k in range(int(f[m-1]), int(f[m])):
+            filter_bank[m-1, k] = 2 * (k - f[m-1]) / ((f[m+1] - f[m-1]) * (f[m] - f[m-1]))
+        for k in range(int(f[m]), int(f[m+1])):
+            filter_bank[m-1, k] = 2 * (f[m+1] - k) / ((f[m+1] - f[m-1]) * (f[m+1] - f[m]))
+    return filter_bank
+
+fl = 133 # Hz
+fh = 6855 # Hz
+fs = 16000 # Hz
+nfft = 1024
+L = 20
+
+filter_bank = mel_filterbank(fl, fh, nfft, fs, L)
+
+fig = plt.figure(figsize=(16, 8))  
+ax = fig.gca()
+for filter in filter_bank:
+    ax.plot(filter) 
+plt.title('Triangular filter banks')
+plt.xlabel('Frequency') 
+plt.ylabel('Filter')
+#plt.show()
+plt.savefig('mel_filterbank.jpg') 
