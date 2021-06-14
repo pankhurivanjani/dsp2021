0e664c682b5eae8e662b63aa2815170d442b5122
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2021-06-14 18:49:49 +0200

    1.4 errors fixed

--- DSP_Assignment_8/ex8.py
+++ DSP_Assignment_8/ex8.py
@@ -44,16 +44,17 @@ frame_width = 44100
 frame_window(point8_emph, frame_shift, frame_width)
 '''
 
-frame_size = 0.025
-frame_stride = 0.01
+frame_size = 0.025 #25 ms
+frame_stride = 0.01 # 10 ms
 
 #pdb.set_trace()
 frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
+# frame_length - 1102, frame_step - 441
 signal_length = len(point8_emph)
 frame_length = int(round(frame_length))
 frame_step = int(round(frame_step))
 num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
-
+# num_frames - 1098
 pad_signal_length = num_frames * frame_step + frame_length
 z = np.zeros((pad_signal_length - signal_length, point8_emph.shape[1]))
 
@@ -63,7 +64,7 @@ pad_signal = np.append(point8_emph, z, axis=0) # Pad Signal to make sure that al
 indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T # [num_frames, frame_length]
 frames = pad_signal[indices.astype(np.int32, copy=False)]
 
-frames *= np.repeat(np.hamming(frame_length)[None], point8_emph.shape[1], axis=0).T
+frames *= np.repeat(np.hamming(frame_length)[None], point8_emph.shape[1], axis=0).T #(1098, 1102, 8)
 
 # 1.3 Mel-Filterbank
 def hz2mel(f_hz):
@@ -92,7 +93,7 @@ def mel_filterbank(fl, fh, nfft, fs, L):
 fl = 133 # Hz
 fh = 6855 # Hz
 fs = 16000 # Hz
-nfft = 256 #1024
+nfft = 512 #1024
 L = 20
 
 filter_bank = mel_filterbank(fl, fh, nfft, fs, L)
@@ -108,8 +109,10 @@ plt.ylabel('Filter')
 plt.savefig('DSP_Assignment_8/mel_filterbank.jpg') 
 
 # 1.4 MFCC Implementation
-#pdb.set_trace()
-mag_frames = np.absolute(np.fft.rfft(frames, nfft))  # Magnitude of the FFT
+pdb.set_trace()
+channel_idx = 0
+mag_frames = np.absolute(np.fft.rfft(frames[...,channel_idx], nfft))  # Magnitude of the FFT,
+# (1098, 1102, 257) if fft over all channels, (1098, 257) if single channel
 pow_frames = ((1.0 / nfft) * ((mag_frames) ** 2))  # Power Spectrum
 
 filter_banks = np.dot(pow_frames, filter_bank.T)
@@ -119,28 +122,13 @@ filter_banks = 20 * np.log10(filter_banks)  # dB
 num_ceps = 12
 #pdb.set_trace()
 mfcc = dct(filter_banks, type=2, axis=1, norm='ortho')[:, 1 : (num_ceps + 1)] # Keep 2-13
-'''
-plt.figure(figsize=(16, 8))  
-plt.plot(point8_emph)
-plt.title('Audio signal after pre-emphasis')
-plt.xlabel('Time (s)') #TODO samples or time?
-plt.ylabel('Amplitude (s)')
-#plt.show()
-plt.savefig('DSP_Assignment_8/audio_signal_emph.jpg')
+# (1098, 12, 20) if fft over all channels, (1098, 12) if single channel
 
-
-from matplotlib import cm
 fig, ax = plt.subplots()
-pdb.set_trace()
-mfcc_data= np.swapaxes(mfcc, 0 ,1)
-cax = ax.imshow(mfcc_data[0], interpolation='nearest', cmap=cm.coolwarm, origin='lower')
-ax.set_title('MFCC')
-plt.show()
-'''
-fig, ax = plt.subplots()
-#mfcc_data = np.swapaxes(mfcc[0], 0 ,1)
-mfcc_data = mfcc[3]
+mfcc_data = np.swapaxes(mfcc, 0 ,1)
 cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower', aspect='auto')
-ax.set_title('MFCC')
+ax.set_title('MFCC Spectrogram')
+ax.set_xlabel('Time') 
+ax.set_ylabel('Frequency')
 #Showing mfcc_data
-plt.show()
+plt.savefig('DSP_Assignment_8/mfcc_spectrogram.jpg') 
